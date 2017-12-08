5491d17a4135942ecb2605bb5f15e08e537340d8
Gaz1676 <gazfleming@gmail.com>
2017-12-08 11:32:57 +0000

    donation client connected to donation-web lab

---
+++ src/services/fixtures.js
@@ -0,0 +1,7 @@
+// removed all the donations, users, candidates
+// we not longer need test data
+export default class Fixtures {
+
+  baseUrl = 'http://localhost:4000';
+  methods = ['Cash', 'PayPal'];
+}
